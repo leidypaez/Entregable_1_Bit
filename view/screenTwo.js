8081c953b8202a5d8ae0c3c539faef98c977de02
leidypaez <leidy16paez@gmail.com>
2021-10-22 05:06:16 -0500

    se suben carpetas de js cada una con su funcionalidad, arreglos en css y booststrap

--- view/screenTwo.js
+++ view/screenTwo.js
@@ -1,32 +1,60 @@
-const text1="¡Da rienda suelta a tu imaginación y crea un mundo único de Sims! Explora y personaliza todos los detalles, desde tus Sims a sus hogares y mucho más. Elige el aspecto, la personalidad y la ropa de tus Sims y decide cómo van a vivir día a día. Diseña y construye casas increíbles para cada familia y luego decóralas con tus muebles y elementos decorativos favoritos. Desplázate a distintos barrios donde podrás conocer a otros Sims y enterarte de sus vidas."
-const text2="En medio de una confusión, tres criminales muy diferentes barajan sus probabilidades de supervivencia y de éxito: Franklin, un estafador callejero en busca de oportunidades de verdad y dinero serio; Michael, profesional ex convicto, cuyo retiro no es tan prometedor como él hubiera esperado; y Trevor, un maníaco violento motivado por la posibilidad de encontrar drogas baratas y el próximo atraco importante. Agotadas las opciones, esta banda lo arriesgará todo en una serie de atrevidos y peligrosos atracos que marcarán sus vidas."
-const text3="El mismo aliento devastador y actitud candente, en alta definición. ¡Spyro regresa en Spyro™ Reignited Trilogy! Reaviva la llama con las versiones remasterizadas de los juegos originales: Spyro™ the Dragon, Spyro™ 2 Ripto's Rage! y Spyro™ Year of the Dragon."
+class Gallery {
 
-const titulo1="Los SIMS 4"
-const titulo2="Grand Theft Auto V"
-const titulo3="Spyro - Reignated Trilogy"
-let selectedPhoto = null;
+  constructor(data, max) {
+      this.columns = document.querySelectorAll('.gallery .col');
+      this.modalContent = document.querySelector('.modal-content');
+      this.plusMax = max || 2;
+      this.data = data;
+      this.build();
+  }
 
-function changeText(foto){
-    selectedPhoto=foto.id;
-    let titulo=document.getElementById('tituloJuego')
-    let texto=document.getElementById('text')
-    let video=document.getElementById('videoContainer').childNodes[1]
-    if (selectedPhoto=="foto1"){
-        titulo.innerHTML=titulo1
-        texto.innerHTML=text1
-        video.src="https://www.youtube.com/embed/z00mK3Pxc8w"
-    } 
-    else if (selectedPhoto=="foto2"){
-        titulo.innerHTML=titulo2
-        texto.innerHTML=text2;
-        video.src="https://www.youtube.com/embed/hvoD7ehZPcM"
-        
-    }
-    else{
-        titulo.innerHTML=titulo3
-        texto.innerHTML=text3;
-        video.src="https://www.youtube.com/embed/orzNU1R4wb8"
-    }
-    
+  elementGallery = (obj) => {
+      return `<div class="gallery__link" id="${obj.id}">
+          <figure class="gallery__thumb">
+              <img loading="lazy" class="gallery__image w-100 shadow-1-strong rounded mb-4" data-mdb-toggle="modal" data-mdb-target="#exampleModal"
+                  src="${obj.urlImagen}"
+                  alt="${obj.title}">
+              <figcaption class="gallery__caption">${obj.title} - ${obj.year}</figcaption>
+          </figure>
+      </div>`;
+  } 
+  
+  build = () => {
+      let counter = 0;
+  
+      for (let index = 0; index < this.columns.length; index++) {
+          for (let i = counter; i < counter + this.plusMax; i++) {
+              this.columns[index].innerHTML += this.elementGallery(this.data[i]);
+          }
+          counter += this.plusMax;
+      }
+
+      this.lister();
+  }
+
+  lister = () => {
+      document.querySelectorAll('.gallery__link')
+      .forEach((element, i) => {
+          element.addEventListener('click', () => {
+              const obj = this.data[i];
+              this.modalContent.innerHTML = `
+                  <div class="modal-header p-1">
+                      <h5 class="modal-title ps-1" id="exampleModalLabel">${obj.title}</h5>
+                      <button type="button" class="btn-close m-0 pr-1" data-mdb-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body p-1">
+                      <div>
+                          <figure class="p-0 m-0">
+                              <img loading="lazy" class="w-100 shadow-1-strong rounded"
+                                  src="${obj.urlImagen}"
+                                  alt="${obj.title}">
+                          </figure>
+                      </div>
+                  </div>`;
+          });
+      });
+  }
+
 }
+
+const gallery = new Gallery(data, 2);
--- view/showScreen.js
+++ view/showScreen.js
@@ -1,6 +1,19 @@
 // muestra la segunda pantalla escondiendo las demas
 document.getElementById('aventure').addEventListener('click', () => {
   document.getElementById("first-screen").style.display = "none";
+  document.getElementById('screen-shooter').style.display = "none";
   document.getElementById('screen-aventure').style.display = "block";
 });
 
+document.getElementById('shooter').addEventListener('click', () => {
+  document.getElementById("first-screen").style.display = "none";
+  document.getElementById('screen-aventure').style.display = "none";
+  document.getElementById('screen-shooter').style.display = "block";
+});
+
+document.getElementById('super-game').addEventListener('click', () => {
+  document.getElementById("first-screen").style.display = "none";
+  document.getElementById('screen-aventure').style.display = "none";
+  document.getElementById('screen-shooter').style.display = "none";
+  document.getElementById('screen-super-game').style.display = "block";
+});
